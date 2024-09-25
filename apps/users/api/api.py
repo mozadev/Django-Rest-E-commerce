8b69a2101d706d8347fadb5a0d2095b896b42b22
mozadev <ceosmore@gmail.com>
2024-09-24 23:49:43 -0500

    validation on a serializers

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, TestUserSerializer
 
 @api_view(['GET', 'POST'])
 def user_api_view(request):
@@ -13,6 +13,16 @@ def user_api_view(request):
         #queryset
         users = User.objects.all()
         users_serializer = UserSerializer(users, many = True)
+
+        test_data = {
+            'name': 'jose',
+            'email': 'jose@gmail.com'
+        }
+        test_user = TestUserSerializer(data = test_data, context = test_data)
+        if test_user.is_valid():
+            print("Pass validations")
+        else:
+            print(test_user.errors)
         
         return Response(users_serializer.data, status=status.HTTP_200_OK   )
     
--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -5,5 +5,29 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
- 
+
+class TestUserSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
+
+    def validate_name(self, value):
+
+        #custom validation
+        if 'jose' in value:
+            raise serializers.ValidationError("This name is not allowed")
+           
+        return value
+    
+    def validate_email(self, value):
+
+        #custom validation
+        if value == '':
+            raise serializers.ValidationError("This have to indicate an email")
         
+        if self.validate_name(self.context['name']) in value:
+            raise serializers.ValidationError('The email can not contain the name')
+        
+        return value
+
+    def validate(self, data):
+        return data  
